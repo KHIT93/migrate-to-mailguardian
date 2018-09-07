548f0e99e877cb7014e1059c143da0a16a6ef4be
Kenneth Hansen <kenneth@khansen-it.dk>
2018-09-07 16:59:48 +0200

    Chunk maillog into blocks of 10000 messages

--- migrate.py
+++ migrate.py
@@ -21,6 +21,10 @@ pgsql_conn = None
 start_time = datetime.datetime.now()
 stop_time = None
 
+def migrate_mail_message(message):
+    pass
+        
+
 if __name__ == "__main__":
     with open(args.mailwatch_config, 'r') as f:
         for line in f:
@@ -64,7 +68,7 @@ if __name__ == "__main__":
     print('Successfully connected to the destination PostgreSQL database for MailGuardian' + chr(13))
     
     print('Successfully connected to source and destination datastorage' + chr(13))
-    pgsql_cursor = pgsql_conn.cursor()
+    #pgsql_cursor = pgsql_conn.cursor()
     mysql_cursor = mysql_conn.cursor()
     print('Getting list of tables from source database' + chr(13))
     mysql_cursor.execute("SHOW TABLES")
@@ -77,84 +81,102 @@ if __name__ == "__main__":
     mysql_cursor.execute("SELECT count(id) as id__count FROM maillog")
     total = mysql_cursor.fetchone()['id__count']
     count = 0
+    messages_sql = []
+    if total > 10000:
+        messages_sql.append("SELECT * FROM maillog LIMIT 10000 order_by timestamp")
+        to_process = 10000
+        while to_process < total:
+            to_add = 10000
+            if to_process + 10000 > total:
+                to_add = total - to_process
+            messages_sql.append("SELECT * FROM maillog LIMIT {0} OFFSET {1}".format(to_add, to_process))
+            to_process += to_add
+    else:
+        messages_sql = ["SELECT * FROM maillog"]
     print('Collecting maillog entries to process' + chr(13))
-    mysql_cursor.execute("SELECT * FROM maillog")
-    for message in mysql_cursor:
-        print('[{0}%] :: Processing message {1}'.format(round((count/total) * 100, 2), message['id']))
-        vals = {
-            'id': str(uuid.uuid4()),
-            'from_address': message['from_address'],
-            'from_domain': message['from_domain'],
-            'to_address': message['to_address'],
-            'to_domain': message['to_domain'],
-            'subject': message['subject'] if message['subject'] else "",
-            'client_ip': message['clientip'],
-            'mailscanner_hostname': message['hostname'],
-            'spam_score': message['sascore'] if message['sascore'] else 0.00,
-            'mcp_score': message['mcpsascore'] if message['mcpsascore'] else 0.00,
-            'timestamp': message['timestamp'],
-            'date': message['date'],
-            'size': message['size'],
-            'token': message['token'],
-            'mailq_id': message['id'],
-            'whitelisted': True if message['spamwhitelisted'] == 1 else False,
-            'blacklisted': True if message['spamblacklisted'] == 1 else False,
-            'is_spam': True if message['issaspam'] == 1 or message['isspam'] == 1 or message['ishighspam'] == 1 else False,
-            'is_mcp': True if message['issamcp'] == 1 or message['ismcp'] == 1 or message['ishighmcp'] == 1 else False,
-            'is_rbl_listed': True if message['isrblspam'] == 1 else False,
-            'quarantined': True if message['quarantined'] == 1 else False,
-            'infected': True if message['virusinfected'] == 1 or message['nameinfected'] == 1 or message['otherinfected'] == 1 else False,
-            'released': True if message['released'] == 1 else False
-        }
-        pgsql_cursor.execute("INSERT INTO mail_message (id, from_address, from_domain, to_address, to_domain, subject, client_ip, mailscanner_hostname, spam_score, timestamp, token, whitelisted, blacklisted, is_spam, is_rbl_listed, quarantined, infected, size, mailq_id, is_mcp, mcp_score, date, released) VALUES(%(id)s, %(from_address)s, %(from_domain)s, %(to_address)s, %(to_domain)s, %(subject)s, %(client_ip)s, %(mailscanner_hostname)s, %(spam_score)s, %(timestamp)s, %(token)s, %(whitelisted)s, %(blacklisted)s, %(is_spam)s, %(is_rbl_listed)s, %(quarantined)s, %(infected)s, %(size)s, %(mailq_id)s, %(is_mcp)s, %(mcp_score)s, %(date)s, %(released)s) RETURNING id", (vals))
-        message_id = pgsql_cursor.fetchone()[0]
-        vals = {
-            'id': str(uuid.uuid4()),
-            'contents': message['headers'],
-            'message_id': message_id
-        }
-        pgsql_cursor.execute("INSERT INTO mail_headers (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
-        vals = {
-            'id': str(uuid.uuid4()),
-            'contents': message['report'],
-            'message_id': message_id
-        }
-        pgsql_cursor.execute("INSERT INTO mail_mailscannerreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
-        vals = {
-            'id': str(uuid.uuid4()),
-            'contents': message['mcpreport'],
-            'message_id': message_id
-        }
-        pgsql_cursor.execute("INSERT INTO mail_mcpreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
-        vals = {
-            'id': str(uuid.uuid4()),
-            'contents': message['rblspamreport'],
-            'message_id': message_id
-        }
-        pgsql_cursor.execute("INSERT INTO mail_rblreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
-        vals = {
-            'id': str(uuid.uuid4()),
-            'contents': message['report'],
-            'message_id': message_id
-        }
-        pgsql_cursor.execute("INSERT INTO mail_spamreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
-        transport_log_cursor = mysql_conn2.cursor(dictionary=True)
-        transport_log_cursor.execute("SELECT * FROM mtalog WHERE msg_id='{0}'".format(message_id))
-        for entry in transport_log_cursor:
+    #mysql_cursor.execute("SELECT * FROM maillog")
+    for query in messages_sql:
+        pgsql_cursor = pgsql_conn.cursor()
+        mysql_cursor = mysql_conn.cursor(dictionary=True)
+        mysql_cursor.execute(query)
+        for message in mysql_cursor.fetchall():
+            print('[{0}%] :: Processing message {1}'.format(round((count/total) * 100, 2), message['id']))
             vals = {
                 'id': str(uuid.uuid4()),
-                'timestamp': entry['timestamp'],
-                'message_id': message_id,
-                'transport_host': entry['host'],
-                'transport_type': entry['type'],
-                'relay_host': entry['relay'],
-                'dsn': entry['dsn'],
-                'dsn_message': entry['status'],
-                'delay': entry['delay']
+                'from_address': message['from_address'],
+                'from_domain': message['from_domain'],
+                'to_address': message['to_address'],
+                'to_domain': message['to_domain'],
+                'subject': message['subject'] if message['subject'] else "",
+                'client_ip': message['clientip'],
+                'mailscanner_hostname': message['hostname'],
+                'spam_score': message['sascore'] if message['sascore'] else 0.00,
+                'mcp_score': message['mcpsascore'] if message['mcpsascore'] else 0.00,
+                'timestamp': message['timestamp'],
+                'date': message['date'],
+                'size': message['size'],
+                'token': message['token'],
+                'mailq_id': message['id'],
+                'whitelisted': True if message['spamwhitelisted'] == 1 else False,
+                'blacklisted': True if message['spamblacklisted'] == 1 else False,
+                'is_spam': True if message['issaspam'] == 1 or message['isspam'] == 1 or message['ishighspam'] == 1 else False,
+                'is_mcp': True if message['issamcp'] == 1 or message['ismcp'] == 1 or message['ishighmcp'] == 1 else False,
+                'is_rbl_listed': True if message['isrblspam'] == 1 else False,
+                'quarantined': True if message['quarantined'] == 1 else False,
+                'infected': True if message['virusinfected'] == 1 or message['nameinfected'] == 1 or message['otherinfected'] == 1 else False,
+                'released': True if message['released'] == 1 else False
             }
-            pgsql_cursor.execute("INSERT INTO mail_transportlog (id, timestamp, message_id, transport_host, transport_type, relay_host, dsn, dsn_message, delay) VALUES(%(id)s, %(timestamp)s, %(message_id)s, %(transport_host)s, %(transport_type)s, %(relay_host)s, %(dsn)s, %(dsn_message)s, %(delay)s)", (vals))
-        pgsql_conn.commit()
-        count += 1
+            pgsql_cursor.execute("INSERT INTO mail_message (id, from_address, from_domain, to_address, to_domain, subject, client_ip, mailscanner_hostname, spam_score, timestamp, token, whitelisted, blacklisted, is_spam, is_rbl_listed, quarantined, infected, size, mailq_id, is_mcp, mcp_score, date, released) VALUES(%(id)s, %(from_address)s, %(from_domain)s, %(to_address)s, %(to_domain)s, %(subject)s, %(client_ip)s, %(mailscanner_hostname)s, %(spam_score)s, %(timestamp)s, %(token)s, %(whitelisted)s, %(blacklisted)s, %(is_spam)s, %(is_rbl_listed)s, %(quarantined)s, %(infected)s, %(size)s, %(mailq_id)s, %(is_mcp)s, %(mcp_score)s, %(date)s, %(released)s) RETURNING id", (vals))
+            message_id = pgsql_cursor.fetchone()[0]
+            vals = {
+                'id': str(uuid.uuid4()),
+                'contents': message['headers'],
+                'message_id': message_id
+            }
+            pgsql_cursor.execute("INSERT INTO mail_headers (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
+            vals = {
+                'id': str(uuid.uuid4()),
+                'contents': message['report'],
+                'message_id': message_id
+            }
+            pgsql_cursor.execute("INSERT INTO mail_mailscannerreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
+            vals = {
+                'id': str(uuid.uuid4()),
+                'contents': message['mcpreport'],
+                'message_id': message_id
+            }
+            pgsql_cursor.execute("INSERT INTO mail_mcpreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
+            vals = {
+                'id': str(uuid.uuid4()),
+                'contents': message['rblspamreport'],
+                'message_id': message_id
+            }
+            pgsql_cursor.execute("INSERT INTO mail_rblreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
+            vals = {
+                'id': str(uuid.uuid4()),
+                'contents': message['report'],
+                'message_id': message_id
+            }
+            pgsql_cursor.execute("INSERT INTO mail_spamreport (id, contents, message_id) VALUES (%(id)s, %(contents)s, %(message_id)s)", (vals))
+            transport_log_cursor = mysql_conn2.cursor(dictionary=True)
+            transport_log_cursor.execute("SELECT * FROM mtalog WHERE msg_id='{0}'".format(message_id))
+            for entry in transport_log_cursor:
+                vals = {
+                    'id': str(uuid.uuid4()),
+                    'timestamp': entry['timestamp'],
+                    'message_id': message_id,
+                    'transport_host': entry['host'],
+                    'transport_type': entry['type'],
+                    'relay_host': entry['relay'],
+                    'dsn': entry['dsn'],
+                    'dsn_message': entry['status'],
+                    'delay': entry['delay']
+                }
+                pgsql_cursor.execute("INSERT INTO mail_transportlog (id, timestamp, message_id, transport_host, transport_type, relay_host, dsn, dsn_message, delay) VALUES(%(id)s, %(timestamp)s, %(message_id)s, %(transport_host)s, %(transport_type)s, %(relay_host)s, %(dsn)s, %(dsn_message)s, %(delay)s)", (vals))
+            pgsql_conn.commit()
+            count += 1
+            pgsql_cursor.close()
+            mysql_cursor.close()
     transport_log_cursor.close()
     pgsql_cursor.close()
     mysql_cursor.close()
@@ -252,7 +274,7 @@ if __name__ == "__main__":
         print('[{0}%] :: Processing user {1}'.format(round((count/total) * 100, 2), message['id']))
         vals = {
             'id': str(uuid.uuid4()),
-            'email': user['username'] if '@' in user['username'] else 'admin@' . user['username'],
+            'email': user['username'] if '@' in user['username'] else 'admin@' + user['username'],
             'first_name': user['fullname'],
             'is_domain_admin': True if user['type'] == 'D' else False,
             'is_staff': True if user['type'] == 'A' else False,
