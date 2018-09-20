1c3df8a69fc583166a516de99092dcf3d1339ba3
Kenneth Hansen <kenneth@khansen-it.dk>
2018-09-20 19:07:54 +0200

    fix domain query list

--- migrate.py
+++ migrate.py
@@ -331,12 +331,12 @@ if __name__ == "__main__":
     pgsql_cursor = pgsql_conn.cursor()
     
     pgsql_cursor.execute("SELECT domain_id FROM core_user_domains")
-    dlist = []
+    dlist = ""
     for domain in pgsql_cursor:
-        dlist.append(domain[0])
+        dlist += "'{0}', ".format(domain[0])
     pgsql_cursor.close()
     pgsql_cursor = pgsql_conn.cursor()
-    pgsql_cursor.execute("SELECT id, name FROM domains_domain WHERE id NOT IN ({0})".format("','".join(dlist)))
+    pgsql_cursor.execute("SELECT id, name FROM domains_domain WHERE id NOT IN ({0})".format(dlist[:-1]))
     for domain in pgsql_cursor:
         errors.append('No users connected to domain {0}. Please check'.format(domain[1]))
 
